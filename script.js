104659d8294236087050c3b774c1ac1a3b2adad1
justincschoi <justincschoi@gmail.com>
2023-07-07 20:11:37 -0400

    create computer choice and play round of rps

---
+++ script.js
@@ -0,0 +1,31 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    if (choice === 1) {
+        return "rock";
+    }
+    else if (choice === 2) {
+        return "paper";
+    }
+    else {
+        return "scissor";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `Draw both played ${playerSelection}`
+    }
+    else if ((playerSelection.toLowerCase() == "rock" && computerSelection == "scissor") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") || playerSelection.toLowerCase() == "scissor" && computerSelection == "paper") {
+        return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}`;
+    }
+    else {
+        return `Computer has won =( Player chose ${playerSelection} and computer chose ${computerSelection}`;
+    }
+
+}
+
+const playerSelection = "rock";
+console.log(playerSelection);
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
