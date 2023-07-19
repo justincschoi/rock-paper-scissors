d39a6ca78587d2f037366ec8d449921823b7ea13
justincschoi <justincschoi@gmail.com>
2023-07-19 17:12:03 -0400

    Remove alert and update with rps buttons

--- script.js
+++ script.js
@@ -11,7 +11,38 @@ function getComputerChoice() {
     }
 }
 
-// function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return `Draw both played ${playerSelection}`
+    }
+    else if ((playerSelection == "rock" && computerSelection == "scissor") || (playerSelection == "paper" && computerSelection == "rock") || playerSelection == "scissor" && computerSelection == "paper") {
+        return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}`;
+    }
+    else {
+        return `Computer has won =( Player chose ${playerSelection} and computer chose ${computerSelection}`;
+    }
+
+}
+
+const computerSelection = getComputerChoice();
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', function (e) {
+        const playerSelection = e.target.className;
+        const computerSelection = getComputerChoice();
+        console.log(playerSelection);
+        console.log(computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+
+    });
+});
+
+
+
+// function game() {
+//     let playerSelection = prompt("Rock paper or scissor?: ");
+//     const computerSelection = getComputerChoice();
+
 //     if (playerSelection == computerSelection) {
 //         return `Draw both played ${playerSelection}`
 //     }
@@ -21,27 +52,12 @@ function getComputerChoice() {
 //     else {
 //         return `Computer has won =( Player chose ${playerSelection} and computer chose ${computerSelection}`;
 //     }
-
+// }
+// for (i = 0; i < 5; i++) {
+//     console.log(game());
 // }
 
-// const playerSelection = "rock";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+// const playerSelection = document.querySelectorAll('button');
+// playerSelection.forEach((selection) => selection.addEventListener('click', function() {
 
-function game() {
-    let playerSelection = prompt("Rock paper or scissor?: ");
-    const computerSelection = getComputerChoice();
-
-    if (playerSelection == computerSelection) {
-        return `Draw both played ${playerSelection}`
-    }
-    else if ((playerSelection.toLowerCase() == "rock" && computerSelection == "scissor") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") || playerSelection.toLowerCase() == "scissor" && computerSelection == "paper") {
-        return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}`;
-    }
-    else {
-        return `Computer has won =( Player chose ${playerSelection} and computer chose ${computerSelection}`;
-    }
-}
-for (i = 0; i < 5; i++) {
-    console.log(game());
-}
+// }));
