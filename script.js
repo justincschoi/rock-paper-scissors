a5bbdd5d8a6bf97d78dedf583af6ce83ade8836a
justincschoi <justincschoi@gmail.com>
2023-07-20 14:36:20 -0400

    Update results to display scoreboard

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3) + 1;
     if (choice === 1) {
-        return "rock";
+        return 'rock';
     }
     else if (choice === 2) {
-        return "paper";
+        return 'paper';
     }
     else {
-        return "scissor";
+        return 'scissor';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
 
-        return `Draw both played ${playerSelection}`
+        return `Draw both played ${playerSelection}\n Player: ${playerWins} vs Computer: ${computerWins}`
     }
-    else if ((playerSelection == "rock" && computerSelection == "scissor") || (playerSelection == "paper" && computerSelection == "rock") || playerSelection == "scissor" && computerSelection == "paper") {
-        return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}`;
+    else if ((playerSelection == 'rock' && computerSelection == 'scissor') || (playerSelection == 'paper' && computerSelection == 'rock') || playerSelection == "scissor" && computerSelection == "paper") {
+        playerWins++;
+        return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}\n Player: ${playerWins} vs Computer: ${computerWins}`;
     }
     else {
-        return `Computer has won. Player chose ${playerSelection} and computer chose ${computerSelection}`;
+        computerWins++;
+        return `Computer has won. Player chose ${playerSelection} and computer chose ${computerSelection}\n Player: ${playerWins} vs Computer: ${computerWins}`;
+    }
+}
+
+function checkWinner() {
+    if (computerWins == 5) {
+        result.innerText += "\nCPU HAS WON";
+        playerWins = 0;
+        computerWins = 0;
+    }
+    else if (playerWins == 5) {
+        result.innerText += "\nPLAYER HAS WON";
+        playerWins = 0;
+        computerWins = 0;
     }
 
 }
 
-const computerSelection = getComputerChoice();
+let computerWins = 0;
+let playerWins = 0;
 const buttons = document.querySelectorAll('button');
 const result = document.getElementById('result')
 buttons.forEach((button) => {
@@ -33,30 +49,7 @@ buttons.forEach((button) => {
         const playerSelection = e.target.className;
         const computerSelection = getComputerChoice();
         result.innerText = playRound(playerSelection, computerSelection);
+        checkWinner();
     });
 });
 
-
-
-// function game() {
-//     let playerSelection = prompt("Rock paper or scissor?: ");
-//     const computerSelection = getComputerChoice();
-
-//     if (playerSelection == computerSelection) {
-//         return `Draw both played ${playerSelection}`
-//     }
-//     else if ((playerSelection.toLowerCase() == "rock" && computerSelection == "scissor") || (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") || playerSelection.toLowerCase() == "scissor" && computerSelection == "paper") {
-//         return `Player has won! Player chose ${playerSelection} and computer chose ${computerSelection}`;
-//     }
-//     else {
-//         return `Computer has won =( Player chose ${playerSelection} and computer chose ${computerSelection}`;
-//     }
-// }
-// for (i = 0; i < 5; i++) {
-//     console.log(game());
-// }
-
-// const playerSelection = document.querySelectorAll('button');
-// playerSelection.forEach((selection) => selection.addEventListener('click', function() {
-
-// }));
