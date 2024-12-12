81ccb6f2dd545518ac0589e90f41e6cf447931e5
Justin Choi <38042156+justincschoi@users.noreply.github.com>
2024-12-11 20:59:40 -0500

    Write logic to play a single round of RPS
    The playRound function will take humanSelection and computerSelection arguments and determine the winner using logical operators. Score variables, humanScore and computerScore, have been added to track the winner of the round using a counter.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -17,3 +20,19 @@ function getHumanChoice() {
 
 const computerSelection = getComputerChoice();
 const humanSelection = getHumanChoice();
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerSelection == 'paper') {
+        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. Human wins!`);
+        humanScore++;
+    }
+    else if (humanChoice == 'rock' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. Computer wins!`);
+        computerScore++;
+    }
+    else {
+        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. DRAW!`);
+    }
+}
+
+playRound(humanSelection, computerSelection);
