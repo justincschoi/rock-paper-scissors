1a4e20120fc370c984cd54ed500267d39936e6cf
Justin Choi <38042156+justincschoi@users.noreply.github.com>
2024-12-11 19:25:00 -0500

    Create function for computer choice
    This function will return rock, paper, or scissors based on a randomly generated number. The result is stored in computerSelection.

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 0) {
+        return "rock";
+    } else if (randomNumber === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+
+}
+
+const computerSelection = getComputerChoice();
