224214f5c4b8bd5c3cf4ae6a6d019e03c6769c0e
Justin Choi <38042156+justincschoi@users.noreply.github.com>
2024-12-11 23:28:24 -0500

    Write logic to play 5 rounds and fix logic typos
    -The playGame function uses a loop to get human and computer selections while calling playRound. 5 rounds will be played afterwards a winner will be determined based on humanScore and computerScore counters.
    -Typos were fixed in the playRound logical operators as the wrong parameters were being used.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -18,21 +15,43 @@ function getHumanChoice() {
     return prompt("Please select: rock, paper or scissors");
 }
 
-const computerSelection = getComputerChoice();
-const humanSelection = getHumanChoice();
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    let round = 0;
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerSelection == 'paper') {
-        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. Human wins!`);
-        humanScore++;
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerChoice == 'paper') {
+            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Human wins!`);
+            humanScore++;
+        }
+        else if (humanChoice == 'rock' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'rock') {
+            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Computer wins!`);
+            computerScore++;
+        }
+        else {
+            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. DRAW!`);
+        }
     }
-    else if (humanChoice == 'rock' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'rock') {
-        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. Computer wins!`);
-        computerScore++;
-    }
-    else {
-        console.log(`Human has selected ${humanSelection} and Computer has selected ${computerSelection}. DRAW!`);
+
+    for (let i = 1; i <= 5; i++) {
+        const computerSelection = getComputerChoice();
+        const humanSelection = getHumanChoice();
+        playRound(humanSelection, computerSelection);
+        round++
+
+        if (round === 5) {
+            if (humanScore > computerScore) {
+                console.log(`Human win! Human has won ${humanScore} while Computer has won ${computerScore}.`);
+            }
+            else if (computerScore > humanScore) {
+                console.log(`Computer win! Computer has won ${computerScore} while human has won ${humanScore}.`);
+            }
+            else {
+                console.log(`DRAW! Human and Computer has both won ${humanScore}.`)
+            }
+        }
     }
 }
 
-playRound(humanSelection, computerSelection);
+playGame();
