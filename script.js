29621aa6c41c169353abbdb2976b8b526005fdb6
Justin Choi <38042156+justincschoi@users.noreply.github.com>
2024-12-25 17:40:36 -0500

    Add event handling for button layout RPS Game
    - Remove logic to play 5 rounds temporarily
    - Use event delegation by selecting btnContainer. This will minimize the amount of event handlers needed.
    - Add event parameter to getHumanChoice function. The function looks at the id attribute of the DOM element through event.target.id and returns the corresponding human choice.
    - Add event parameter to playRound function. The playRound function will pass the click event to the getHumanChoice function once called upon by the event listeners on the buttons.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelector('.btnContainer');
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
 
@@ -11,47 +13,37 @@ function getComputerChoice() {
 
 }
 
-function getHumanChoice() {
-    return prompt("Please select: rock, paper or scissors");
-}
+function getHumanChoice(event) {
+    let target = event.target;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let round = 0;
-
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerChoice == 'paper') {
-            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Human wins!`);
-            humanScore++;
-        }
-        else if (humanChoice == 'rock' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'rock') {
-            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Computer wins!`);
-            computerScore++;
-        }
-        else {
-            console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. DRAW!`);
-        }
-    }
-
-    for (let i = 1; i <= 5; i++) {
-        const computerSelection = getComputerChoice();
-        const humanSelection = getHumanChoice();
-        playRound(humanSelection, computerSelection);
-        round++
-
-        if (round === 5) {
-            if (humanScore > computerScore) {
-                console.log(`Human win! Human has won ${humanScore} while Computer has won ${computerScore}.`);
-            }
-            else if (computerScore > humanScore) {
-                console.log(`Computer win! Computer has won ${computerScore} while human has won ${humanScore}.`);
-            }
-            else {
-                console.log(`DRAW! Human and Computer has both won ${humanScore}.`)
-            }
-        }
+    switch (target.id) {
+        case 'rock':
+            return 'rock';
+            break;
+        case 'paper':
+            return 'paper';
+            break;
+        case 'scissors':
+            return 'scissors';
+            break;
     }
 }
 
-playGame();
+function playRound(event) {
+    let humanChoice = getHumanChoice(event);
+    let computerChoice = getComputerChoice();
+
+    if (humanChoice == 'rock' && computerChoice == 'scissors' || humanChoice == 'paper' && computerChoice == 'rock' || humanChoice == 'scissors' && computerChoice == 'paper') {
+        console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Human wins!`);
+
+    }
+    else if (humanChoice == 'rock' && computerChoice == 'paper' || humanChoice == 'paper' && computerChoice == 'scissors' || humanChoice == 'scissors' && computerChoice == 'rock') {
+        console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. Computer wins!`);
+
+    }
+    else {
+        console.log(`Human has selected ${humanChoice} and Computer has selected ${computerChoice}. DRAW!`);
+    }
+}
+
+buttons.addEventListener('click', playRound);
